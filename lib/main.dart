8d18487514f4f1d068a0eb715ed36509ac81dce7
João Couto <setemais07@gmail.com>
2026-01-09 18:52:23 -0300

    Fix: Build error by removing unsupported loadingBuilder in CachedNetworkImage

--- lib/main.dart
+++ lib/main.dart
@@ -4904,18 +4904,7 @@ class _ProfileCardState extends State<ProfileCard> {
                     child: CircularProgressIndicator(),
                   );
                 },
-                loadingBuilder: (context, child, loadingProgress) {
-                  if (loadingProgress == null) return child;
-                  return Center(
-                    child: CircularProgressIndicator(
-                      value: loadingProgress.expectedTotalBytes != null
-                          ? loadingProgress.cumulativeBytesLoaded /
-                              loadingProgress.expectedTotalBytes!
-                          : null,
-                      color: Colors.white,
-                    ),
-                  );
-                },
+
               )
             else
               Container(
